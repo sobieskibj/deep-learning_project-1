0e16ce569f749cc570b5aaeadebbd008a4322071
wladekolejnik <wladekolejnik@gmail.com>
2023-03-22 14:11:28 +0100

    Delete num_workers from exp3

--- experiments/_3/exp.py
+++ experiments/_3/exp.py
@@ -69,7 +69,6 @@ if __name__ == '__main__':
         'dataloader': {
             'batch_size': 128,
             'shuffle': True,
-            'num_workers': 8
         },
         'training': {
             'device': torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'),
